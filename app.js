fbbecf89a777823be44d404ca8fcf03a8b02d811
Kris Streeter <krisstreeter@Kris-Streeters-MacBook-Air.local>
2019-05-26 16:54:55 -0700

    testing

--- app.js
+++ app.js
@@ -5,12 +5,13 @@
     // Get the value of the input field with id="numb"
     x = document.getElementById("numb").value;
   
+
     // If x is Not a Number or less than one or greater than 10
     if (isNaN(x) || x < 1 || x > 10000) {
       text = "Did you type all their names? I meant enter just the number sorry.";
     } else {
         //change to es6 later
-      text = (x * 5) + "? Am I right? Ish?";
+      text = "You're " + (x * 5) + "? Am I right? Ish?";
     } // add more conditions for if its under 2 or so, about being weirdly young 
         // add something if it's a high number 
     document.getElementById("demo").innerHTML = text;
